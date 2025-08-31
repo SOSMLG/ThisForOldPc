b74c76c5d98facceeb6f92b473df2a3921983f0a
SOSMLG <117748270+SOSMLG@users.noreply.github.com>
2025-08-31 20:41:42 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
+#backports
+"deb http://deb.debian.org/debian/ trixie-backports main non-free-firmware"
+"deb-src http://deb.debian.org/debian/ trixie-backports main non-free-firmware"
+#installing the packages 
 sudo apt update
-sudo apt upgrade
-sudo apt install 
+sudo apt upgrade -yy
+sudo apt install vlc micro thunar libreoffice icewm fastfetch alsa-utils pulseaudio pavucontrol unzip cargo p7zip ntfs-3g eza zoxide vlc gimp imagemagick fzf ffmpeg fonts-firacode fonts-jetbrains-mono fonts-croscore fonts-crosextra-carlito fonts-crosextra-caladea fonts-noto fonts-noto-cjk git falkon -yy
+#micro plugins
+micro -plugin install filemanager  
+micro -plugin install fzf           
+micro -plugin install quoter        
+micro -plugin install autoclose     
+micro -plugin install detectindent  
+micro -plugin install linter        
+micro -plugin install go            
+micro -plugin install fish
